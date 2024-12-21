3df5eeddbc4d2ee859a71088901e244e855fefd9
Hughgz <minhhieu.swe@gmail.com>
2024-12-22 04:29:18 +0700

    first-commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Vo Minh Hieu </span>
+            <br />
+            I am currently studying software engineering, focusing on Java
+            backend development.
+            <br />
+            I am working on improving my problem-solving and data structure
+            skills, and I am eager to learn new technologies and grow as a
+            developer.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Football
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Marvel
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Listen to music
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            " If you're not smarter than others, you have to put in more time
+            than they do!"{" "}
+          </p>
+          <footer className="blockquote-footer">Minh Hieu</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I am passionate about <span className="purple">Java Backend Development</span> and have extensive experience in this field. 
+              <br />
+              <br />
+              <b className="purple">Backend Development Skills:</b>
+              <i>
+                <b className="purple">
+                  {" "}Java, Spring MVC, Spring Security, RESTful API, MySQL, MSSQL, MongoDB, Docker, Azure.
+                </b>
+              </i>
+              <br />
+              <br />
+              My field of interest lies in building scalable backend systems and ensuring high-quality code using 
+              <span className="purple"> testing tools</span> like <b>Postman</b> and <b>Selenium</b>.
+              <br />
+              <br />
+              I also have experience working in an Agile environment using <b className="purple">Scrum</b> methodologies and tools like <b>JIRA</b>.
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/Hughgz"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/your-twitter-profile"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/your-linkedin-profile"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/your-instagram-profile"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
